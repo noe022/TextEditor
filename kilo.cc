3fd786f05794b58e86776f3c4c7e934a2e8a2247
noe022 <noeliaperezmojica@gmail.com>
2026-04-08 01:02:17 +0100

    Raw Mode implemented

--- kilo.cc
+++ kilo.cc
@@ -1,7 +1,24 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios termios_struct;
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_struct);
+}
+
+void enableRawMode(){
+	tcgetattr(STDIN_FILENO, &termios_struct);
+	atexit(disableRawMode);
+
+	//Hacemos una copia de termios
+	struct termios copy_struct = termios_struct;
+	copy_struct.c_lflag &= ~ECHO;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &copy_struct);
+}
+
 int main() {
+	enableRawMode();
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
 		return 0;
