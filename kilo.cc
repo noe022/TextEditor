6875ec41c740ca885e30e154cb3f03f527644219
noe022 <noeliaperezmojica@gmail.com>
2026-04-10 18:24:24 +0100

    Raw mode done, max time, error control

--- kilo.cc
+++ kilo.cc
@@ -1,32 +1,47 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 struct termios termios_struct;
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_struct);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_struct) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode(){
-	tcgetattr(STDIN_FILENO, &termios_struct);
+	if (tcgetattr(STDIN_FILENO, &termios_struct) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
 	struct termios copy_struct = termios_struct;
-	copy_struct.c_lflag &= ~(ECHO | ICANON);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &copy_struct);
+  copy_struct.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	copy_struct.c_cflag &= ~(OPOST);
+	copy_struct.c_cflag &= ~(CS8);
+	copy_struct.c_lflag &= ~(ECHO | IEXTEN |ICANON | ISIG);
+  copy_struct.c_cc[VMIN] = 0;
+  copy_struct.c_cc[VTIME] = 1;
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &copy_struct) == -1) die ("tcsetattr");
 }
 
 int main() {
 	enableRawMode();
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-		if (iscntrl(c)) {
-    	printf("%d\n", c);
-		} else {
-			printf("%d ('%c')\n", c, c);
-		}
-		return 0;
-	}
+  while (true) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
+	return 0;
 }
